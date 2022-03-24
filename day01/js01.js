145db28cc039185e438032a395986ef778eef250
AUTUMN <krshlee22@naver.com>
2022-03-24 22:31:24 +0900

    first commit

---
+++ day01/js01.js
@@ -0,0 +1,91 @@
+// var hello;
+// hello = 'hi';
+// console.log(hello);
+
+// const my_name = "KWON";
+// console.log(my_name);
+
+// let numberType = 1;
+// let bigIntType = 12222222222222222222222222222222222222222n;
+// let stringType = "Hello";
+// let boolType = true;
+// let nullType = null;
+// let undefinedType;
+// console.log(typeof bigIntType);
+
+// console.log(2 > 3);
+// console.log('Z' > 'C');
+// console.log('5' > 2);
+// console.log('33' == 33);
+// console.log('33' === 33);
+
+// console.log(0 == false); // 
+// console.log('' == false); // 
+// console.log(0 === false); // 
+// console.log(null === undefined); // 
+// console.log(null == undefined); // 
+
+// function hello(str) {
+//     console.log(`hello ${str}`);
+// }
+// hello("world");
+
+// function hi() {
+//     console.log('hi world');
+// }
+// hi();
+
+// let world = "";
+// function hi() {
+//     let hello = "안녕";   // 지역 변수
+//     world = " 세상";
+//     console.log(hello + world);
+// }
+// hi();
+// console.log(hello);
+
+// function hi(){}
+// let test = hi();
+// console.log(test);
+
+// let hi = function() {
+//     console.log("hello");
+// }
+// hi();
+
+// function hi() {
+//     console.log("hello");
+// }
+// let hi2 = hi;
+// hi2();
+// hi();
+
+// hi();
+// function hi() {
+//     console.log("hello");
+// }
+
+// var num = 1;
+// function number(){
+//     var num = 2;
+//     console.log(num);
+// }
+// number();
+// console.log(num);
+
+// var num = 1;
+// function one() {
+//     var num = 10;
+//     two();
+// }
+// function two() {
+//     console.log(num);
+// }
+// one();
+// two();
+
+// console.log("시작");
+// setTimeout(function(){
+//     console.log("3초후 실행");
+// }, 3000);
+// console.log("끝");
